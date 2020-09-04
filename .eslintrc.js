dad34a3ca94e6a48eeb1fe70aecac456d21233e7
wuxiaoquan <somerq@qq.com>
2020-09-04 11:10:39 +0800

    feat: add file

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb",
+        "airbnb/hooks",
+        "plugin:react/recommended",
+        'plugin:unicorn/recommended',
+        'plugin:promise/recommended',
+        "plugin:@typescript-eslint/recommended",
+        'prettier',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+        'prettier/unicorn',
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        'unicorn',
+        'promise',
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'import/extensions': [
+          'ERROR',
+          'ignorePackages',
+          {
+            ts: 'never',
+            tsx: 'never',
+            json: 'never',
+            js: 'never',
+          },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.tsx', '.ts', '.js', '.json'],
+            },
+        },
+    },    
+};
